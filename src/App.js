25661e90e49b78e2d72d8a991ca12beff5df5b8b
Fred Guthrie <fred3@myself.com>
2020-04-01 14:41:31 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -6,23 +6,27 @@ import DataBody      from "./components/DataBody";
 import DataPortfolio from "./components/DataPortfolio";
 import Footer        from "./components/footer";
 // import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Wrapper>
         <Nav />
         <Switch>
-          <Route exact path="/React-portfolio/" component= {DataBody} />
+          {/* <Route exact path="/React-portfolio/" component= {DataBody} />
           <Route exact path="/React-portfolio/portFolio" component={DataPortfolio} />
           <Route exact path="/React-portfolio/contact" component={DataContact} />
-          <Route path="/React-portfolio/" render={() => <div>404 Error Page not found</div>} />
-        </Switch>
+          <Route path="/React-portfolio/" render={() => <div>404 Error Page not found</div>} /> */}
+        <Route exact path="/" component= {DataBody} />
+          <Route exact path="/portFolio" component={DataPortfolio} />
+          <Route exact path="/contact" component={DataContact} />
+          <Route path="/" render={() => <div>404 Error Page not found</div>} />
+      </Switch>
         <Footer />
       </Wrapper>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,25 +13,25 @@ function Nav() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
           <li id="NavAbout" className={
-                window.location.pathname === "/React-portfolio/" 
+                window.location.pathname === "/" 
                   ? "nav-link active"
                   : "nav-link"
               }>
-            <a className="nav-link About" href="/React-portfolio/">About </a>
+            <a className="nav-link About" href="/">About </a>
           </li>
           <li id="NavContact" className={
-                window.location.pathname === "/React-portfolio/contact"
+                window.location.pathname === "/contact"
                   ? "nav-link active"
                   : "nav-link"
               }>
-            <a className="nav-link " href="/React-portfolio/contact">Contact <span class="sr-only">(current)</span></a>
+            <a className="nav-link " href="/contact">Contact <span class="sr-only">(current)</span></a>
           </li>
           <li  id="NavPortfolio" className={
-                window.location.pathname === "/React-portfolio/portFolio" 
+                window.location.pathname === "/portFolio" 
                   ? "nav-link active"
                   : "nav-link"
               }>
-            <a className="nav-link " href="/React-portfolio/portFolio">Portfolio <span class="sr-only">(current)</span></a>
+            <a className="nav-link " href="/portFolio">Portfolio <span class="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
