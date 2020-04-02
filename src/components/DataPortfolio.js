1dd07a54ef1f815ba1244e649fe8cbcb5c84ca4f
Fred Guthrie <fred3@myself.com>
2020-04-02 15:17:14 -0500

    blank the code

--- src/components/DataPortfolio.js
+++ src/components/DataPortfolio.js
@@ -10,95 +10,96 @@ import workday from "../photo/workday.png";
 const DataPortfolio = () => {
 
 
-    return (
+     return (
 
-        <picture className="mt-5 row justify-content-center">
-            <div data-aos="fade-up-right" className="col-lg-5 col-md-5 col-sm-12 mb-3">
-                <div className="col lg-12">
-                    <td>
-                        <img id="port" src={APIQUIZ}  className="card-img" alt="fredGquiz" />
-                    </td>
-                    <div className="card-img-overlay">
-                        <div className=" mt-5">
-                            <a href="https://github.com/fred-smu/APICodeQuiz/" className="btn btn-lg btn-block text-dark">Quiz
-                            Game</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    //     <picture className="mt-5 row justify-content-center">
+    //         <div data-aos="fade-up-right" className="col-lg-5 col-md-5 col-sm-12 mb-3">
+    //             <div className="col lg-12">
+    //                 <td>
+    //                     <img id="port" src={APIQUIZ}  className="card-img" alt="fredGquiz" />
+    //                 </td>
+    //                 <div className="card-img-overlay">
+    //                     <div className=" mt-5">
+    //                         <a href="https://github.com/fred-smu/APICodeQuiz/" className="btn btn-lg btn-block text-dark">Quiz
+    //                         Game</a>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
 
 
-            <div data-aos="fade-up-right" className="col-lg-5 col-md-5 col-sm-12 mb-3">
-                <div className="col lg-12">
-                    <td>
-                        <img id="port" src={weatherDash} className="card-img" alt="fredGweather" />
-                    </td>
-                    <div className="card-img-overlay">
-                        <div className="h-100 mt-5">
-                            <a href="https://github.com/fred-smu/weatherDash/"
-                                className="btn text-dark btn-lg btn-block">Weather App</a>
-                        </div>
-                    </div>
+    //         <div data-aos="fade-up-right" className="col-lg-5 col-md-5 col-sm-12 mb-3">
+    //             <div className="col lg-12">
+    //                 <td>
+    //                     <img id="port" src={weatherDash} className="card-img" alt="fredGweather" />
+    //                 </td>
+    //                 <div className="card-img-overlay">
+    //                     <div className="h-100 mt-5">
+    //                         <a href="https://github.com/fred-smu/weatherDash/"
+    //                             className="btn text-dark btn-lg btn-block">Weather App</a>
+    //                     </div>
+    //                 </div>
 
-                </div>
-            </div>
-            <div data-aos="fade-up-left" className="col-lg-5 col-md-5 col-sm-12 mb-3">
+    //             </div>
+    //         </div>
+    //         <div data-aos="fade-up-left" className="col-lg-5 col-md-5 col-sm-12 mb-3">
 
-                <div className="col lg-12">
-                    <td>
-                        <img id="port" src= {tempEngine} className="card-img" alt="fredGEmployee" />
-                    </td>
-                    <div className="card-img-overlay">
-                        <div className="h-100 mt-5">
-                            <a href="https://github.com/fred-smu/Template-Engine---Employee-Summary"
-                                className="btn  btn-lg btn-block text-light">Template-Engine---Employee-Summary</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    //             <div className="col lg-12">
+    //                 <td>
+    //                     <img id="port" src= {tempEngine} className="card-img" alt="fredGEmployee" />
+    //                 </td>
+    //                 <div className="card-img-overlay">
+    //                     <div className="h-100 mt-5">
+    //                         <a href="https://github.com/fred-smu/Template-Engine---Employee-Summary"
+    //                             className="btn  btn-lg btn-block text-light">Template-Engine---Employee-Summary</a>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
      
-            <div data-aos="fade-up-left" className="col-lg-5 col-md-5 col-sm-12 mb-3">
-                <div className="col lg-12">
-                    <td>
-                        <img id="port" src= {workday} className="card-img" alt="FredGplanner" />
-                    </td>
-                    <div className="card-img-overlay">
-                        <div className="h-100 mt-5">
-                            <a href="https://github.com/fred-smu/WorkDayScheduler/"
-                                className="btn btn-lg btn-block text-dark">Day Planner</a>
-                        </div>
-                    </div>
+    //         <div data-aos="fade-up-left" className="col-lg-5 col-md-5 col-sm-12 mb-3">
+    //             <div className="col lg-12">
+    //                 <td>
+    //                     <img id="port" src= {workday} className="card-img" alt="FredGplanner" />
+    //                 </td>
+    //                 <div className="card-img-overlay">
+    //                     <div className="h-100 mt-5">
+    //                         <a href="https://github.com/fred-smu/WorkDayScheduler/"
+    //                             className="btn btn-lg btn-block text-dark">Day Planner</a>
+    //                     </div>
+    //                 </div>
 
-                </div>
-            </div>
-            <div data-aos="fade-up-right" className="col-lg-5 col-md-5 col-sm-12 mb-3">
-                <div className="col lg-12">
-                    <td>
-                        <img id="port" src= {family} className="card-img" alt="fredGlifestyle" />
-                    </td>
-                    <div className="card-img-overlay">
-                        <div className="h-100 mt-5">
-                            <a href className="btn btn-lg btn-block text-light"> My Family</a>
-                        </div>
-                    </div>
+    //             </div>
+    //         </div>
+    //         <div data-aos="fade-up-right" className="col-lg-5 col-md-5 col-sm-12 mb-3">
+    //             <div className="col lg-12">
+    //                 <td>
+    //                     <img id="port" src= {family} className="card-img" alt="fredGlifestyle" />
+    //                 </td>
+    //                 <div className="card-img-overlay">
+    //                     <div className="h-100 mt-5">
+    //                         <a href className="btn btn-lg btn-block text-light"> My Family</a>
+    //                     </div>
+    //                 </div>
 
-                </div>
-            </div>
-            <div data-aos="fade-up-left" className="col-lg-5 col-md-5 col-sm-12 mb-3">
-                <div className="col lg-12">
-                    <td>
-                        <img id="port" src= {dogs} className="card-img" alt="fredGlifestyle" />
-                    </td>
-                    <div className="card-img-overlay">
-                        <div className="h-100 mt-5">
-                            <a href className="btn btn-lg btn-block text-light"> Bonnie and Misty</a>
-                        </div>
-                    </div>
+    //             </div>
+    //         </div>
+    //         <div data-aos="fade-up-left" className="col-lg-5 col-md-5 col-sm-12 mb-3">
+    //             <div className="col lg-12">
+    //                 <td>
+    //                     <img id="port" src= {dogs} className="card-img" alt="fredGlifestyle" />
+    //                 </td>
+    //                 <div className="card-img-overlay">
+    //                     <div className="h-100 mt-5">
+    //                         <a href className="btn btn-lg btn-block text-light"> Bonnie and Misty</a>
+    //                     </div>
+    //                 </div>
 
-                </div>
-            </div>
-       </picture>
-        
+    //             </div>
+    //         </div>
+    //    </picture>
+        <h1> it works</h1>
+
 
     );
 };
