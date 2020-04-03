4708df346e461b2561caf5ebe6954b6d539dd676
Fred Guthrie <fred3@myself.com>
2020-04-02 21:07:19 -0500

    nav fix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
           <Route exact path="/portFolio" component={DataPortfolio} />
           {/* <Route exact path="https://fred-smu.github.io/portFolio" component={DataPortfolio} /> */}
           <Route exact path="/contact" component={DataContact} />
-          <Route path="/" render={() => <div>404 Error Page not found</div>} />
+          <Route path="/" render={() => <div>{Route.path}  404 Error Page not found</div>} />
         </Switch>
         <Footer />
       </Wrapper>
