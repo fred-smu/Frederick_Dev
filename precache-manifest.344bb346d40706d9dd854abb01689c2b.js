a741821423e0e657e2cb243a2a764f3fb598284d
Fred Guthrie <fred3@myself.com>
2020-04-09 23:59:27 -0500

    Updates

---
+++ precache-manifest.344bb346d40706d9dd854abb01689c2b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecbb490774332d51906395fec23537b",
+    "url": "/Frederick_Dev/index.html"
+  },
+  {
+    "revision": "d5375be2123d744ab362",
+    "url": "/Frederick_Dev/static/css/main.44956dc4.chunk.css"
+  },
+  {
+    "revision": "7f80f6a3ea1e5f94224c",
+    "url": "/Frederick_Dev/static/js/2.0d9c4736.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Frederick_Dev/static/js/2.0d9c4736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5375be2123d744ab362",
+    "url": "/Frederick_Dev/static/js/main.1c4d0cda.chunk.js"
+  },
+  {
+    "revision": "105f8cd757414d4f46a2",
+    "url": "/Frederick_Dev/static/js/runtime-main.45e4d0f6.js"
+  },
+  {
+    "revision": "19a9dee3cf9667cabef572da910f1986",
+    "url": "/Frederick_Dev/static/media/APIQUIZ.19a9dee3.png"
+  },
+  {
+    "revision": "133f4deb3cf8dea8971e5783ad84a5e6",
+    "url": "/Frederick_Dev/static/media/bonnie & misty.png.133f4deb.jpg"
+  },
+  {
+    "revision": "c383b5847ac1e81aa0837c0d39af29d8",
+    "url": "/Frederick_Dev/static/media/family.c383b584.png"
+  },
+  {
+    "revision": "df830afb6c3f331c8f9ef01ed4b6b957",
+    "url": "/Frederick_Dev/static/media/resback.df830afb.png"
+  },
+  {
+    "revision": "85e78c7d9a2caa683d52ab31ceff5ae2",
+    "url": "/Frederick_Dev/static/media/tempEngine.85e78c7d.png"
+  },
+  {
+    "revision": "cd849962f3ae7db49d6ab2b1799addc4",
+    "url": "/Frederick_Dev/static/media/tranback.cd849962.png"
+  },
+  {
+    "revision": "6f5b63c9be090e9a99e8fb9df28be64a",
+    "url": "/Frederick_Dev/static/media/weatherDash.6f5b63c9.png"
+  },
+  {
+    "revision": "17250412284996619c387b1674b0d089",
+    "url": "/Frederick_Dev/static/media/workday.17250412.png"
+  }
+]);
